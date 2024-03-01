0366fcb3afa52fff2415a669cd5f4ee2535f0175
Ilia Grigoriev <grigoriev.ilia1@yandex.ru>
2024-03-01 17:10:23 +0300

    feat: add a scheduled method, register it

---
+++ scrapper/src/main/java/edu/java/scrapper/LinkUpdateScheduler.java
@@ -0,0 +1,19 @@
+package edu.java.scrapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@EnableScheduling
+@ConditionalOnProperty(value = "app.scheduler.enable", havingValue = "true")
+public class LinkUpdateScheduler {
+
+    @Scheduled(fixedDelayString = "#{@scheduler.interval}")
+    public void update() {
+        log.info("A message from scheduled method.");
+    }
+}
