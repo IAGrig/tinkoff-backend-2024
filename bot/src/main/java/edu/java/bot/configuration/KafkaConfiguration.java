1743185918a53273303e63c29b72bda1cf7fb4d8
Ilia Grigoriev <grigoriev.ilia1@yandex.ru>
2024-04-09 17:48:01 +0300

    fix(bot): fix configs, add updates handling and missing annotation

--- bot/src/main/java/edu/java/bot/configuration/KafkaConfiguration.java
+++ bot/src/main/java/edu/java/bot/configuration/KafkaConfiguration.java
@@ -3,36 +3,31 @@ package edu.java.bot.configuration;
 import edu.java.dto.LinkUpdateRequest;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.IntegerSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+@RequiredArgsConstructor
 @Configuration
 public class KafkaConfiguration {
-    @Value("${app.kafka-queue.bootstrap-server:localhost:9092}")
-    private String bootstrapServer;
-    @Value("${app.kafka-queue.consumer-updates-group-id:updates}")
-    private String groupId;
-    @Value("${app.kafka-queue.auto-offset-reset:latest}")
-    private String autoOffsetResetmode;
-    @Value("${app.kafka-queue.enable-auto-commit:false}")
-    private boolean autoCommit;
+    private final ApplicationConfig applicationConfig;
 
     @Bean
     public ConsumerFactory<Integer, LinkUpdateRequest> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetResetmode);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, applicationConfig.kafkaQueue().bootstrapServer());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, applicationConfig.kafkaQueue().consumerUpdatesGroupId());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, applicationConfig.kafkaQueue().autoOffsetReset());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, applicationConfig.kafkaQueue().enableAutoCommit());
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, LinkUpdateRequest.class.getName());
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
@@ -43,6 +38,7 @@ public class KafkaConfiguration {
         ConcurrentKafkaListenerContainerFactory<Integer, LinkUpdateRequest> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(applicationConfig.kafkaQueue().ackMode());
         return factory;
     }
 }
--- bot/src/main/java/edu/java/bot/services/KafkaListenerService.java
+++ bot/src/main/java/edu/java/bot/services/KafkaListenerService.java
@@ -1,19 +1,23 @@
 package edu.java.bot.services;
 
 import edu.java.dto.LinkUpdateRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Log4j2
+@RequiredArgsConstructor
 @Service
 public class KafkaListenerService {
+    private final UpdateService updateService;
+
     @KafkaListener(topics = "${app.kafka-queue.topic-name:botUpdates}", containerFactory = "listenerContainerFactory")
     public void listen(@Payload LinkUpdateRequest request, Acknowledgment acknowledgment) {
-        log.info(request);
+        log.info("Send update about the {} to {}", request.getUrl().toString(), request.getTgChatIds().toString());
+        updateService.processUpdateRequest(request);
         acknowledgment.acknowledge();
     }
 }
